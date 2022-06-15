6ad0afc4fd3ea91735d49c4f6b93f699a0b78eed
Petrick Grillo <petrick.grillo@gmail.com>
2022-06-15 18:46:50 -0300

    Imagem add, porem incompleto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import ImagensProdutos from "./components/ImagensProdutos/meteorito.jpg"
+import Imagens from './components/Imagem/Imagens'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ImagemME = styled.img`
+height:100px;
+`
+
+class App extends React.Component {
+  state = {
+    arrayProdutos: [
+      {
+        nome: "meteorito 1",
+        preco: "50",
+        imagem: ImagensProdutos
+      },
+      {
+        nome: "meteorito 2",
+        preco: "150"
+      },
+      {
+        nome: "meteorito 3",
+        preco: '550'
+      },
+      {
+        nome: "meteorito 4",
+        preco: '1050'
+
+      },
+    ]
+  }
+  render() {
+    console.log(this.state.arrayProdutos)
+    return (
+      <div>
+        {/* {this.state.arrayProdutos.map((produto) => {
+        <section>
+            <p>{produto.nome}</p>
+            <ImagemME src={produto.imagem} alt={produto.nome} />
+          </section>
+        })
+        } */}
+
+        {this.state.arrayProdutos.map((produto) => {
+          return <p>{produto.nome}</p>
+        })}
+        {this.state.arrayProdutos.map((produto) => {
+          return <ImagemME src={produto.imagem} alt= {produto.nome} />
+        })}
+      </div>
+    )
+  }
 }
 
-export default App;
+
+export default App
---
+++ src/components/Imagem/Imagens.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+display:flex;
+`
+const ImagemME = styled.img`
+
+`
+class Imagens extends React.Component {
+  render(){
+    return(
+      <MainContainer>
+
+      </MainContainer>
+    )
+  }
+}
+
+export default Imagens
